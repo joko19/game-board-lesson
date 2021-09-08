72eda4e9e09c78d140569c306bf387ecce0bf947
joko19 <jokoslamet459@gmail.com>
2021-09-08 14:07:13 +0700

    gh-pages config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+
+const isProd = process.env.NODE_ENV === 'production'
+const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
+
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/game-board-lesson' :'',
+  basePath: isProd ? '/game-board-lesson' : '',
+  images: {
+    loader: 'imgix'
+  },
 }
