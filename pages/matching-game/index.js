a85cc8ae268f1be16bbec04d93947ccccb9ab028
joko19 <jokoslamet459@gmail.com>
2021-09-29 01:37:15 +0700

    fix bug

--- pages/matching-game/index.js
+++ pages/matching-game/index.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <div style={{ textAlign: 'center' }}><iframe src="https://www.educandy.com/site/resource_embedded.php?activity-code=e2b95" width="100%" height="640" onload="this.style.height = this.contentWindow.document.body.scrollHeight + 50 + 'px';" style={{ borderWidth: 'thin', }}></iframe></div>
+      <div style={{ textAlign: 'center' }}><iframe src="https://www.educandy.com/site/resource_embedded.php?activity-code=e2b95" width="100%" height="640" onLoad="this.style.height = this.contentWindow.document.body.scrollHeight + 50 + 'px';" style={{ borderWidth: 'thin', }}></iframe></div>
       {/* <MatchingPair /> */}
     </div>
   )
